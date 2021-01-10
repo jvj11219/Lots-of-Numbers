d397fbb16b6ed1412ab6800d64b6863e3f30babe
jvj11219 <77215168+jvj11219@users.noreply.github.com>
2021-01-10 00:14:10 -0600

    Create LotsOfNumbers.py

---
+++ LotsOfNumbers.py
@@ -0,0 +1,53 @@
+def func1(arg1, arg2):
+    var7 = func2(arg2, arg1)
+    def func3(arg8, arg9):
+        result = (arg1 | arg2 & -764) | (399667273 & arg2 + (-651905370 ^ ((1029831321 | 975 + arg1) + arg8)) - var7) - arg9
+        return result
+    var10 = func3(arg2, var7)
+    var15 = func4(arg1, arg2)
+    if arg1 < var10:
+        var20 = class5()
+    else:
+        var20 = class7()
+    for var21 in xrange(50):
+        var22 = var20.func6
+        var22(arg2, var10)
+    var28 = func9(var15, var7)
+    var29 = var15 ^ var28
+    var30 = ((arg2 & var28 - 1891266024) + ((-723288638 & var29) ^ -943 | arg2 + var10 - var29)) + (((-742 - var15 - (360 + (arg2 + arg1)) | (var15 - arg2 | var28) | arg1) ^ -114 ^ 340856724 - var28) | var10)
+    var31 = arg2 + var7
+    var32 = (arg2 - (var7 ^ var30) | var31 | arg2 | (var29 | var28)) | ((var28 ^ (((var7 ^ 417054731 - (var29 + 742 ^ var29 - arg1 + var31 ^ (-704170916 ^ arg1) | var15)) - -368 - var28) + arg1)) & var15)
+    var33 = var30 & var15
+    result = (var33 + var29) ^ var30
+    return result
+def func9(arg23, arg24):
+    var25 = 797 ^ -2109217926
+    var26 = arg23 - 1206519209 | (var25 & arg24) - ((477 + ((((arg23 | (652132002 ^ (446569557 + -174659651) | 244 + (745073914 + var25 - arg23 & arg24 & arg24))) ^ -1659242945 & var25) & -1156040542) + arg24 ^ arg24)) ^ arg24) - arg24
+    var27 = (920 | (arg24 - (arg23 & 1702576840 - var26)) + (arg24 ^ 1311685760 + (var25 - arg24))) - var26 & (121 | arg23 - (((var26 | 800866000 - arg23) | arg23) + arg24)) - var26
+    result = (-523 + var25) ^ (((arg23 | var27 | var25 | -453 & arg23) - var25) & 568)
+    return result
+class class7(object):
+    def func6(self, arg18, arg19):
+        result = ((0 & arg18) | 1856536293 ^ (1 + arg18)) & -1
+        return result
+class class5(object):
+    def func6(self, arg16, arg17):
+        return 0
+def func4(arg11, arg12):
+    var13 = 0
+    for var14 in xrange(19):
+        var13 += (arg11 & 3) & arg12
+    return var13
+def func2(arg3, arg4):
+    var5 = 0
+    for var6 in range(15):
+        var5 += arg3 ^ var6 + var6
+    return var5
+if __name__ == "__main__":
+    print 'prog_size: 5'
+    print 'func_number: 10'
+    print 'arg_number: 34'
+    for i in xrange(25000):
+        x = 5
+        x = func1(x, i)
+        print x,
